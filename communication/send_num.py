ff7284f4563ef7f84b2bed85ecd78f64099d0c3d
xwsa568 <xwsa568@gmail.com>
2023-08-16 00:34:12 +0900

    python code for sending number to esp32c3-softap

--- communication/send_num.py
+++ communication/send_num.py
@@ -1,11 +1,17 @@
-import serial
-import time
+import socket
 
-arduino = serial.Serial('', 11520) #port num, baud rate
+ESP32_AP_IP = "192.168.4.1"  # ESP32-C3 SoftAP IP 주소
+ESP32_PORT = 80
 
-while 1:
-    var = input()
-    var = var.encode('utf-8')
-    arduino.write(var)
-    time.sleep(1)
+def sendNum():
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((ESP32_AP_IP, ESP32_PORT))
+    
+    try:
+        while True:
+            data = input("Enter a number to send: ")  # Mac에서 보낼 숫자 입력
+            client_socket.send(data.encode())
+            print("Data sent:", data)
+    except KeyboardInterrupt:
+        client_socket.close()
 
